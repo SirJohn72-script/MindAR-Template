d9728474f3322efce74afca06e48ae24e8c08ff5
sirJohn <sirJohn72@outlook.es>
2022-08-14 16:55:56 -0500

    docs: script MindAR documented

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const THREE = window.MINDAR.IMAGE.THREE
 
+/* Is important to import the cnd of MindAR*/
+//<script src="https://cdn.jsdelivr.net/npm/mind-ar@1.1.5/dist/mindar-image-three.prod.js"></script>
+
 document.addEventListener('DOMContentLoaded', () => {
   const start = async () => {
     const mindARThree = new window.MINDAR.IMAGE.MindARThree({
@@ -7,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
       imageTargetSrc: './image-tracking/targets.mind',
     })
 
+    /* There is no need to create the rendere, scene, and camera*/
     const { renderer, scene, camera } = mindARThree
     const geometry = new THREE.PlaneGeometry(1, 1)
     const material = new THREE.MeshBasicMaterial({
@@ -15,8 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
       opacity: 0.4,
     })
     const plane = new THREE.Mesh(geometry, material)
+
+    /* three.js group element */
     const anchor = mindARThree.addAnchor(0)
-    anchor.group.add(plane) /* three.js group element */
+    anchor.group.add(plane)
 
     /* Trigger the AR Engine */
     await mindARThree.start()
